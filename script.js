1b8decdeaf9734ca579120cedaa2679344d53ca2
doncik2009 <doniyr.khamrayev@mail.ru>
2025-02-10 18:01:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+/////////////////////////////////task 1////////////////////////////////////////
+const object1 = {
+    name: "doni",
+    age: 15,
+    moreInfo: {
+        city: "Samarkand",
+        country: "Uzbekistan"
+    }
+};
+
+console.log(object1);
+
+
+
+
+const object2 = {
+    name: "Andreww",
+    age: 15,
+    moreInfo: {
+        city: "Ottava",
+        country: "Canada"
+    },
+    
+};
+
+console.log(object2);
+
+
+/////////////////////////////////task 2 ///////////////////////////////////////////
+const newObject = { object1, object2 };
+console.log(newObject);
+
+
+/////////////////////////////////task 3 ///////////////////////////////////////////
+const keys = Object.keys(newObject);
+console.log(keys);
+
+/////////////////////////////////task 4 ///////////////////////////////////////////
+const values = Object.values(newObject);
+console.log(values);
+
+
+
